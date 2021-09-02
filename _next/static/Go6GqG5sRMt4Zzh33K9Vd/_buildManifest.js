2c181ae64847582002a7e7028d0f1370ea632fd7
Aditya Mulgundkar <adityamulgundkar.nmims@gmail.com>
2021-09-02 12:37:31 +0530

    Updates

---
+++ _next/static/Go6GqG5sRMt4Zzh33K9Vd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-49b054c16bfd0f729ec6.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
